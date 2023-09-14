e76f33ba8a4485f43d1b8b363caf09021640c1d6
KlayRodrigs <KlayvertGab@protonmail.com>
2023-09-14 17:43:54 -0300

    first commit

---
+++ bin/calculadora_dio.dart
@@ -0,0 +1,5 @@
+import 'package:calculadora_dio/calculadora_dio.dart' as calculadora_dio;
+
+void main(List<String> arguments) {
+  print('Hello world: ${calculadora_dio.calculate()}!');
+}
---
+++ test/calculadora_dio_test.dart
@@ -0,0 +1,8 @@
+import 'package:calculadora_dio/calculadora_dio.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
